01966c816f4089b25d2d028a96fbe92abf6c5872
HuyVu-Dev <vuquanghuy@gmail.com>
2022-04-06 14:06:22 +0700

    Init project websocket

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const WebSocket = require("ws");
+const http = require("http");
+
+const app = express();
+const server = http.createServer(app);
+const PORT = 8080;
+
+const wss = new WebSocket.Server({ server });
+wss.on("connection", (ws) => {
+  ws.on("message", (data, isBinary) => {
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data, { binary: isBinary });
+      }
+    });
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server started on port ${server.address().port}`);
+});
