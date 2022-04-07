24e3c1c1313e51741395c3d9397d0a58bc90fd3a
HuyVu-Dev <vuquanghuy@gmail.com>
2022-04-07 08:42:43 +0700

    Add logger

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const WebSocket = require("ws");
 const http = require("http");
+const logger = require("./modules/logger");
 
 const app = express();
 const server = http.createServer(app);
@@ -11,11 +12,21 @@ app.use(express.static(__dirname + "/"));
 const wss = new WebSocket.Server({ server });
 wss.on("connection", (ws) => {
   ws.on("message", (data, isBinary) => {
+    var rawData = JSON.parse(data);
+    if (rawData.type === "message") {
+      logger.info(
+        `${rawData.owner} (${rawData.type}) sent: ${rawData.message}`
+      );
+    }
     wss.clients.forEach(function each(client) {
       if (client.readyState === WebSocket.OPEN) {
         client.send(data, { binary: isBinary });
       }
     });
+  });
+
+  ws.on("error", (ws, err) => {
+    logger.error(err);
   });
 });
 
